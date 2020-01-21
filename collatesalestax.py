26500a02f4d09ee1cb97d0387aa127ac8e2acf9b
Scott Degen <taco_taster@hotmail.com>
2020-01-21 10:48:28 -0600

    Update for new report format.

--- collatesalestax.py
+++ collatesalestax.py
@@ -36,6 +36,7 @@ def getOrders(reader):
                 yield order
                 order = None
             order = Order(newOrderID)
+        '''
         shipZip = row[35]
         if shipZip != "":
             order.shipZip = shipZip
@@ -43,6 +44,17 @@ def getOrders(reader):
         jurisdictionName=row[45] #Jurisdiction_Name
         taxableAmountString= row[64].strip()
         taxWithheldString= row[59].strip() #Taxable_Amount
+        taxableAmount = Decimal(taxableAmountString) if taxableAmountString != '' else 0
+        taxWithheld = Decimal(taxWithheldString) if taxWithheldString != '' else 0 
+        '''
+        
+        shipZip = row[34]
+        if shipZip != "":
+            order.shipZip = shipZip
+        jurisdictionLevel= row[38].lower().strip()
+        jurisdictionName=row[39] #Jurisdiction_Name
+        taxableAmountString= row[58].strip() #Taxable_Amount
+        taxWithheldString= row[52].strip() #Tax_Amount
         taxableAmount = Decimal(taxableAmountString) if taxableAmountString != '' else 0
         taxWithheld = Decimal(taxWithheldString) if taxWithheldString != '' else 0 
         
